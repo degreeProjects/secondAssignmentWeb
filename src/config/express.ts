e33d343878ebed9efb44ced3d51a9d196faec6e8
benbenSapir <benbenfsapir@gmail.com>
2025-12-11 14:33:20 +0200

    feat: swagger

--- src/config/express.ts
+++ src/config/express.ts
@@ -1,7 +1,8 @@
-import { Express } from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import { Express } from "express";
 import { configRoutes } from "./routes";
+import { configSwagger } from "./swagger";
 
 export const configExpress = (app: Express) => {
   app.use(bodyParser.json());
@@ -9,4 +10,5 @@ export const configExpress = (app: Express) => {
   app.use(cors());
 
   configRoutes(app);
+  configSwagger(app);
 };
